0d88f8d4dbcd91052f76054b8f5b933d585e5aa4
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2015-05-19 13:04:14 +0200

    Added operator precedence though while loops.

--- Parsing/ITI.Parser.Tests/TokenizerTests.cs
+++ Parsing/ITI.Parser.Tests/TokenizerTests.cs
@@ -4,30 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace ITI.Parser.Tests
 {
     [TestFixture]
     public class TokenizerTests
     {
-        [TestCase( "3" )]
-        [TestCase( "3 8" )]
-        [TestCase( "3 8 12 8787 3712" )]
-        public void list_of_numbers( string toParse )
+        [TestCase("3")]
+        [TestCase("3 8")]
+        [TestCase("3 8 12 8787 3712")]
+        public void list_of_numbers(string toParse)
         {
-            var t = new StringTokenizer( toParse );
+            var t = new StringTokenizer(toParse);
             List<double> values = new List<double>();
             t.GetNextToken();
-            while( t.CurrentToken != TokenType.EndOfInput )
+            while (t.CurrentToken != TokenType.EndOfInput)
             {
-                Assert.That( t.CurrentToken == TokenType.Number );
+                Assert.That(t.CurrentToken == TokenType.Number);
                 double v;
-                Assert.That( t.MatchDouble( out v ) );
-                values.Add( v );
+                Assert.That(t.MatchDouble(out v));
+                values.Add(v);
             }
-            CollectionAssert.AreEqual( toParse.Split( ' ' ).Select( s => Double.Parse( s ) ).ToList(), values );
+            CollectionAssert.AreEqual(toParse.Split(' ').Select(s => Double.Parse(s)).ToList(), values);
         }
 
+        [Test]
+        public void analyserTest()
+        {
+            Analyser a = new Analyser();
+            var tokenizer = new StringTokenizer("3 + 5 * 125 / 7 - 6 + 10");
 
+            Node n = a.Analyse(tokenizer);
+            Console.WriteLine(n.ToString());
+
+            a = new Analyser();
+            tokenizer = new StringTokenizer("1-1+1");
+
+            n = a.Analyse(tokenizer);
+            Console.WriteLine(n.ToString());
+            //Debugger.Break();
+        }
     }
 }
--- Parsing/ITI.Parser/Analyser/Analyser.cs
+++ Parsing/ITI.Parser/Analyser/Analyser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,139 @@ namespace ITI.Parser
 
         public Node Analyse( ITokenizer tokenizer )
         {
+            //return ReadExpr(tokenizer);
+
+            if (tokenizer.CurrentToken == TokenType.None) tokenizer.GetNextToken();
+            return HandleExpression(tokenizer);
+        }
+
+        Node HandleExpression(ITokenizer tokenizer)
+        {
+            Node term = HandleTerm(tokenizer);
+
+            TokenType opToken = tokenizer.CurrentToken;
+
+            Node expr = term;
+
+            if((opToken & TokenType.IsAddOperator) == TokenType.IsAddOperator)
+            {
+                while((opToken & TokenType.IsAddOperator) == TokenType.IsAddOperator)
+                {
+                    tokenizer.GetNextToken();
+                    Node right = HandleTerm(tokenizer);
+                    expr = new BinaryNode(opToken, expr, right);
+                    opToken = tokenizer.CurrentToken;
+                }
+                return expr;
+            }
+            else 
+            {
+                return term;
+            }
+        }
+
+        Node HandleTerm(ITokenizer tokenizer) {
+            Node factor = HandleFactor(tokenizer);
+
+            TokenType opToken = tokenizer.CurrentToken;
+
+            Node expr = factor;
+
+            if ((opToken & TokenType.IsMultOperator) == TokenType.IsMultOperator)
+            {
+                while ((opToken & TokenType.IsMultOperator) == TokenType.IsMultOperator)
+                {
+                    tokenizer.GetNextToken();
+                    Node right = HandleFactor(tokenizer);
+                    expr = new BinaryNode(opToken, expr, right);
+
+                    opToken = tokenizer.CurrentToken;
+                }
+
+                return expr;
+            }
+            else
+            {
+                return factor;
+            }
+
+        }
+
+        Node HandleFactor(ITokenizer tokenizer) {
+            TokenType t = tokenizer.CurrentToken;
+
+            bool minus = false;
+            if (t == TokenType.Minus) { minus = true; tokenizer.GetNextToken(); }
+
+            double v;
+            if(tokenizer.MatchDouble(out v))
+            {
+                return new ConstantNode(minus?-v:v);
+            } else if(tokenizer.CurrentToken == TokenType.OpenPar)
+            {
+                tokenizer.GetNextToken();
+                Node d = HandleExpression(tokenizer);
+                if(tokenizer.CurrentToken != TokenType.ClosePar)
+                {
+                    return new ErrorNode(String.Format("Mismatched close paren in factor: Expected CloseParen but for {0}", tokenizer.CurrentToken));
+                } else
+                {
+                    return d;
+                }
+            } else
+            {
+                return new ErrorNode(String.Format("Invalid token in factor: Expected Number or OpenParen but got {0}", tokenizer.CurrentToken));
+            }
+        }
+
+        private Node ReadExpr(ITokenizer tokenizer)
+        {
+            Node left;
+            TokenType operatorToken;
+            Node right;
+            
+            // Left
+            TokenType token = tokenizer.GetNextToken();
+            double constValue;
+            if (token == TokenType.OpenPar)
+            {
+                left = ReadExpr(tokenizer);
+            }
+            else if(tokenizer.MatchDouble(out constValue))
+            {
+                left = new ConstantNode(constValue);
+            }
+            else
+            {
+                return new ErrorNode(String.Format("Invalid left token:", tokenizer.CurrentToken.ToString()));
+            }
+
+            // Operator
+            operatorToken = tokenizer.CurrentToken;
+
+            if ((operatorToken & TokenType.IsAddOperator) == TokenType.IsAddOperator || (operatorToken & TokenType.IsMultOperator) == TokenType.IsMultOperator)
+            {
+                Debug.Assert(left != null);
+
+                return new BinaryNode(operatorToken, left, ReadExpr(tokenizer));
+            }
+            //else if ( operatorToken == TokenType.EndOfInput || operatorToken == TokenType.ClosePar )
+            //{
+            //    return left;
+            //}
+            else
+            {
+                if (left == null)
+                {
+                    return new ErrorNode(String.Format("Invalid operator: {0}", tokenizer.CurrentToken));
+                }
+
+                if (operatorToken == TokenType.ClosePar)
+                {
+                    operatorToken = tokenizer.GetNextToken();
+                }
+                return left;
+            }
 
         }
     }
